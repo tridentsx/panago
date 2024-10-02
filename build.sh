99c3b12503f39e1d4a22e9be66244a4eb3e43bbd
cf <carl-fredrik.sundstrom@ericsson.com>
2024-10-02 17:32:00 +0700

    Fixed the bash script for local build

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 # Linux AMD64
-GOOS=linux GOARCH=amd64 go build -o myapp-linux-amd64
+GOOS=linux GOARCH=amd64 go build -o dist/panago-linux-amd64
 
 # Windows ARM64
-GOOS=windows GOARCH=arm64 go build -o myapp-windows-arm64.exe
+GOOS=windows GOARCH=amd64 go build -o dist/panago-windows-amd64.exe
 
-# macOS AMD64
-GOOS=darwin GOARCH=amd64 go build -o myapp-darwin-amd64
+# macOS AMD64 and ARM64
+GOOS=darwin GOARCH=amd64 go build -o dist/panago-darwin-amd64
+GOOS=darwin GOARCH=arm64 go build -o dist/panago-darwin-arm64
